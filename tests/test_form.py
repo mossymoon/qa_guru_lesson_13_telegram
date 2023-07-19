ff0afd1505746725e73b45775294b0e4a10e9b84
Stepan Sergienko <stephan.sergienko@gmail.com>
2023-07-19 08:44:40 +0300

    added_new_files

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,3 +1,5 @@
+import allure
+
 from pages.demoqa_form import RegistrationPage
 from pages.registration_page import UserPageFillForm
 
@@ -5,8 +7,11 @@ from pages.registration_page import UserPageFillForm
 def test_user_registration():
     new_user = UserPageFillForm()
     registration = RegistrationPage()
-    registration.open()
-    registration.fill_registration_form(user=new_user)
-    registration.submit_form()
-    registration.validate_form(user=new_user)
-    registration.close_validation_window()
+    with allure.step("Open registrations form"):
+        registration.open()
+    with allure.step("Fill form"):
+        registration.fill_registration_form(user=new_user)
+        registration.submit_form()
+    with allure.step("Check form results"):
+        registration.validate_form(user=new_user)
+        registration.close_validation_window()
