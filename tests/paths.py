d437881fa35c3ca64f0d19635e3a8bfc3f64004e
Stepan <96680106+mossymoon@users.noreply.github.com>
2023-07-29 17:58:18 +0300

    Delete paths.py

--- tests/paths.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def get_path_to_photo(name_file):
-    abs_path = os.path.abspath(__file__)
-    dir_tests = os.path.dirname(abs_path)
-    return os.path.join(dir_tests, "resources", name_file)
